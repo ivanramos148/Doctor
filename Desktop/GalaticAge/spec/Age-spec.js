141cd87b9538a7d5e723ae149718f3f11daababf
ivanramos148248@gmail.com <ivanramos148248@gmail.com>
2018-03-18 20:43:08 -0700

    made working src

--- Desktop/GalaticAge/spec/Age-spec.js
+++ Desktop/GalaticAge/spec/Age-spec.js
@@ -1,9 +1,8 @@
  import { Age } from './../src/Age.js'
 
-describe('', function() {
-
-  it('', function() {
-    // var example = new Constructor(parameters)
-    // expect(example.method()).toEqual('');
+describe('Age', function() {
+  it('should take the users age and make in into second', function() {
+    let input = new Age(1)
+    expect(input.yearToSeconds()).toEqual(31557459.167);
   });
 });

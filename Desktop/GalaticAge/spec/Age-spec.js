26f9258f5efea83c5f1c9a803e6b713fd37be54e
ivanramos148248@gmail.com <ivanramos148248@gmail.com>
2018-03-18 18:22:15 -0700

    made working test file and src

---
+++ Desktop/GalaticAge/spec/Age-spec.js
@@ -0,0 +1,9 @@
+ import { Age } from './../src/Age.js'
+
+describe('', function() {
+
+  it('', function() {
+    // var example = new Constructor(parameters)
+    // expect(example.method()).toEqual('');
+  });
+});
